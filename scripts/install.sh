8d409fbca0b565832d9e1ef23b73f3734cb7176e
Dave Glover <dglover@microsoft.com>
2019-10-07 15:46:29 +1100

    doco

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,20 +2,17 @@
 
 while true; do
     echo ""
-    read -p "Kubernetes Master or Node Set Up? ([M]aster, [N]ode), [Q]uit: " PyLab_Setup_Mode
-    case $PyLab_Setup_Mode in
+    read -p "Kubernetes Master or Node Set Up? ([M]aster, [N]ode), or [Q]uit: " Kube_Setup_Mode
+    case $Kube_Setup_Mode in
         [Mm]* ) break;;
         [Nn]* ) break;;
         [Qq]* ) exit 1;;
-        * ) echo "Please answer yes(y), no(n), or quit(q).";;
+        * ) echo "Please answer [M]aster, [N]ode), or [Q]uit.";;
     esac
 done
 
-if [ $PyLab_Setup_Mode = 'M' ] || [ $PyLab_Setup_Mode = 'm' ]; then   
+if [ $Kube_Setup_Mode = 'M' ] || [ $Kube_Setup_Mode = 'm' ]; then   
     ./install-master-node.sh
 else 
     ./install-node.sh
 fi
-
-
-
