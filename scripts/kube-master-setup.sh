01ce24e80a22ccf37711d189ab51e06f33a042a1
Dave Glover <dglover@microsoft.com>
2019-10-07 14:29:27 +1100

    remove Pi Sense patch

--- scripts/kube-master-setup.sh
+++ scripts/kube-master-setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Patch for Pi Sense HAT on Raspbian Buster
-sudo raspi-config nonint do_resolution 2 4
-sudo sed -i 's/#hdmi_force_hotplug=1/hdmi_force_hotplug=1/g' /boot/config.txt
-
 # Rename your pi
 read -p "Name your Raspberry Pi (eg k8smaster, k8snode1, ...): " RPINAME
 sudo raspi-config nonint do_hostname $RPINAME
