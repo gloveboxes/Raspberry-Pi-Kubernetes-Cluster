8af07fef6be2fdd50fd0263ef472a1ca03e26793
Dave Glover <dglover@microsoft.com>
2019-10-19 10:19:11 +1100

    doco

--- scripts/install-kube-master.sh
+++ scripts/install-kube-master.sh
@@ -12,6 +12,9 @@ sudo apt-get install -qy kubeadm
 echo -e "\nPulling Kubernetes Images - this will take a few minutes depending on network speed.\n"
 kubeadm config images pull
 
+
+echo -e "\nInitialising Kubernetes Master - This will take a few minutes. Be patient:)\n"
+
 # Set up Kubernetes Master Node
 sudo kubeadm init --apiserver-advertise-address=192.168.100.1 --pod-network-cidr=10.244.0.0/16 --token-ttl 0
 
