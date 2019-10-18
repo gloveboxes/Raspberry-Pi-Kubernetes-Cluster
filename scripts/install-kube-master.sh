c32c29c71c9db035009c86b68b541be1a141ae6d
Dave Glover <dglover@microsoft.com>
2019-10-19 10:43:07 +1100

    doco

--- scripts/install-kube-master.sh
+++ scripts/install-kube-master.sh
@@ -27,6 +27,8 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 
+cd ~/Raspberry-Pi-Kubernetes-Cluster-master/kubesetup
+
 echo -e "\nInstalling Flannel\n"
 
 # Install Flannel
@@ -37,7 +39,7 @@ kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/2140ac876ef134
 # Install MetalLB LoadBalancer
 # https://metallb.universe.tf
 kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
-kubectl apply -f ../kubesetup/metallb/metallb.yml
+kubectl apply -f ./metallb/metallb.yml
 
 echo -e "\nInstalling MetalLB LoadBalancer\n"
 
@@ -45,17 +47,17 @@ echo -e "\nInstalling MetalLB LoadBalancer\n"
 # https://kubernetes.io/docs/tasks/access-application-cluster/web-ui-dashboard/
 # https://medium.com/@kanrangsan/creating-admin-user-to-access-kubernetes-dashboard-723d6c9764e4
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta4/aio/deploy/recommended.yaml
-kubectl apply -f ../kubesetup/dashboard/dashboard-admin-user.yml
-kubectl apply -f ../kubesetup/dashboard/dashboard-admin-role-binding.yml
+kubectl apply -f ./dashboard/dashboard-admin-user.yml
+kubectl apply -f ./dashboard/dashboard-admin-role-binding.yml
 
 echo -e "\nInstalling Persistent Storage Support\n"
 
 ## Enable Persistent Storage
-kubectl apply -f ../kubesetup/persistent-storage/nfs-client-deployment-arm.yaml
-kubectl apply -f ../kubesetup/persistent-storage/storage-class.yaml
-kubectl apply -f ../kubesetup/persistent-storage/persistent-volume.yaml
-kubectl apply -f ../kubesetup/persistent-storage/persistent-volume-claim.yaml
-kubectl apply -f ../kubesetup/persistent-storage/nginx-test-pod.yaml
+kubectl apply -f ./persistent-storage/nfs-client-deployment-arm.yaml
+kubectl apply -f ./persistent-storage/storage-class.yaml
+kubectl apply -f ./persistent-storage/persistent-volume.yaml
+kubectl apply -f ./persistent-storage/persistent-volume-claim.yaml
+kubectl apply -f ./persistent-storage/nginx-test-pod.yaml
 
 ## Remove install restart after reboot
 sed --in-place '/~\/Raspberry-Pi-Kubernetes-Cluster-master\/scripts\/install-kube-master.sh/d' ~/.bashrc
