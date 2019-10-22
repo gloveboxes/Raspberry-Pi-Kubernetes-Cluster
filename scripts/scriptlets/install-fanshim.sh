700ffd710320b212e4f8ed079e7cae3d0ed18207
Dave Glover <dglover@microsoft.com>
2019-10-23 10:31:01 +1100

    scriptlets

---
+++ scripts/scriptlets/install-fanshim.sh
@@ -0,0 +1,44 @@
+sleep 2 # let system settle
+
+while : ;
+do
+    sudo apt install -y python3-pip
+    if [ $? -eq 0 ]
+    then
+        break
+    else
+        echo "\nFanSHIM installation failed. Retrying in 10 Seconds\n"
+        sleep 10
+    fi
+done
+
+cd ~/
+
+while : ;
+do
+    wget https://github.com/pimoroni/fanshim-python/archive/master.zip
+    if [ $? -eq 0 ]
+    then
+        break
+    else
+        echo "\nFanSHIM installation failed. Retrying in 10 Seconds\n"
+        sleep 10
+    fi
+done
+
+
+unzip ~/master.zip
+rm ~/master.zip
+
+cd fanshim-python-master
+
+sudo ./install.sh
+if [ $? -ne 0 ]
+then
+    echo "\nFanSHIM installation failed. Retrying in 10 Seconds\n"
+    sleep 10
+    continue
+fi
+
+cd examples
+sudo ./install-service.sh --on-threshold 65 --off-threshold 55 --delay 2
