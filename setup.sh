22016722327ab924b247c09bac0ad94af3e9969a
Dave Glover <dglover@microsoft.com>
2019-10-28 09:18:08 +1100

    doco

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ cd ~/
 
 wget -q https://github.com/gloveboxes/Raspberry-Pi-Kubernetes-Cluster/archive/master.zip
 
+sudo apt update && sudo apt install -y unzip sshpass
+
 sudo rm -r -f ~/Raspberry-Pi-Kubernetes-Cluster-master
 unzip -qq master.zip
 rm master.zip
