12fc1292cd2d1382d9d239823eef84163e62374e
Dave Glover <dglover@microsoft.com>
2019-10-28 10:18:51 +1100

    doco

--- scripts/scriptlets/master/install-dhcp-server.sh
+++ scripts/scriptlets/master/install-dhcp-server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SCRIPTS_DIR="~/Raspberry-Pi-Kubernetes-Cluster-master/scripts/scriptlets"
-
 # Install DHCP Server
 while : ;
 do
@@ -23,8 +21,7 @@ sudo sed -i 's/INTERFACESv4=""/INTERFACESv4="eth0"/g' /etc/default/isc-dhcp-serv
 sudo sed -i 's/INTERFACESv6=""/INTERFACESv4="eth0"/g' /etc/default/isc-dhcp-server > /dev/null
 
 # Append required dhcp config to system config
-echo "$SCRIPTS_DIR/master/dhcpd.conf" 
-cat $SCRIPTS_DIR/master/dhcpd.conf | sudo tee -a /etc/dhcp/dhcpd.conf > /dev/null
+cat ~/Raspberry-Pi-Kubernetes-Cluster-master/scripts/scriptlets/master/dhcpd.conf | sudo tee -a /etc/dhcp/dhcpd.conf > /dev/null
 
 echo -e "\nStarting DHCP Server\n"
 
