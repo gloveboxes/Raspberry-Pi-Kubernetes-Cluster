d3c0e6ed9b4497a0ae3f7cc39894bc1022fd1f75
Dave Glover <dglover@microsoft.com>
2019-10-28 11:38:03 +1100

    doco

--- scripts/scriptlets/master/kubernetes-init.sh
+++ scripts/scriptlets/master/kubernetes-init.sh
@@ -1,13 +1,14 @@
 # Preload the Kubernetes images
-echo -e "\nPulling Kubernetes Images - this will take a few minutes depending on network speed.\n"
-kubeadm config images pull
+# echo -e "\nPulling Kubernetes Images - this will take a few minutes depending on network speed.\n"
+# kubeadm config images pull
 
 echo -e "\nInitialising Kubernetes Master - This will take a few minutes. Be patient:)\n"
 
 # Set up Kubernetes Master Node
 sudo kubeadm init --apiserver-advertise-address=192.168.100.1 --pod-network-cidr=10.244.0.0/16 --token-ttl 0 --skip-token-print
 
-# make kubectl generally avaiable
+# make kubectl generally available
+rm -r -f $HOME/.kube
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
