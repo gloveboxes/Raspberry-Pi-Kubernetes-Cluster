e05b08be9022b183e3e9734a30db10dae9e35205
Dave Glover <dglover@microsoft.com>
2019-10-31 20:25:58 +1100

    doco

--- scripts/scriptlets/common/boot-from-usb.sh
+++ scripts/scriptlets/common/boot-from-usb.sh
@@ -2,10 +2,21 @@
 echo -e "\nEnabling boot from USB Drive\n"
 
 sudo sfdisk --delete /dev/sda
+
+sleep 6
+
 echo 'type=83' | sudo sfdisk /dev/sda
+
+lsblk
+sleep 6
+
 sudo sfdisk -d /dev/sda
 
 sudo mkfs.ext4 /dev/sda1
+
+lsblk
+sleep 6
+
 sudo mkdir /media/usbdrive
 sudo mount /dev/sda1 /media/usbdrive
 sudo rsync -avx / /media/usbdrive
