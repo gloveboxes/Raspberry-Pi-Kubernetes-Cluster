9af4086191ae41a43a8614e3097ae218baeb7660
Dave Glover <dglover@microsoft.com>
2019-11-01 18:04:48 +1100

    doco

--- scripts/install-master.sh
+++ scripts/install-master.sh
@@ -103,7 +103,7 @@ function Generate_SSH() {
     echo -e "You will be prompted for the Raspberry Pi password. The default password is 'raspberry'\n"
 
     ssh-keygen -f "/home/pi/.ssh/known_hosts" -R "$ipaddress" &> /dev/null
-    ssh-keyscan -H $ipaddress >> ~/.ssh/known_hosts  # https://www.techrepublic.com/article/how-to-easily-add-an-ssh-fingerprint-to-your-knownhosts-file-in-linux/
+    ssh-keyscan -H $ipaddress >> ~/.ssh/known_hosts > /dev/null  # https://www.techrepublic.com/article/how-to-easily-add-an-ssh-fingerprint-to-your-knownhosts-file-in-linux/
 
     ssh-copy-id -i ~/.ssh/id_rsa_rpi_kube_cluster pi@$ipaddress 
 }
