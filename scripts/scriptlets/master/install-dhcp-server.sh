43c2e2fe2b8907b336735b95a4f73e1c49315236
Dave Glover <dglover.au@hotmail.com>
2020-05-02 21:57:03 +1000

    disabled INTERFACESv6="eth0"
    Starting ISC DHCPv4 server: dhcpddhcpd service already running (pid file /var/run/dhcpd.pid currenty exists) ... failed!
    https://www.raspberrypi.org/forums/viewtopic.php?t=210310

--- scripts/scriptlets/master/install-dhcp-server.sh
+++ scripts/scriptlets/master/install-dhcp-server.sh
@@ -4,7 +4,8 @@ sudo service isc-dhcp-server stop
 
 # bind IPV4 to eth0
 sudo sed -i 's/INTERFACESv4=""/INTERFACESv4="eth0"/g' /etc/default/isc-dhcp-server > /dev/null
-sudo sed -i 's/INTERFACESv6=""/INTERFACESv6="eth0"/g' /etc/default/isc-dhcp-server > /dev/null
+# https://www.raspberrypi.org/forums/viewtopic.php?t=210310
+# sudo sed -i 's/INTERFACESv6=""/INTERFACESv6="eth0"/g' /etc/default/isc-dhcp-server > /dev/null
 
 # Append required dhcp config to system config
 cat ~/Raspberry-Pi-Kubernetes-Cluster-master/scripts/scriptlets/master/dhcpd.conf | sudo tee -a /etc/dhcp/dhcpd.conf > /dev/null
