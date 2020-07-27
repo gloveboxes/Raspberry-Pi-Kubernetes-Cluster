ed8ced063f46cda51b031b526b0c323b42e837eb
Dave Glover <dglover@microsoft.com>
2020-07-27 20:36:32 +1000

    docker config for systemd

--- scripts/scriptlets/common/install-docker.sh
+++ scripts/scriptlets/common/install-docker.sh
@@ -16,6 +16,18 @@ then
             sleep 10
         fi
     done
+    
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
 fi
 
 sudo reboot
